c69e1046898832a793488dbc27fa0103620171c4
Dhruba Mitra <dhruba10414@gmail.com>
2020-09-14 20:47:18 +0600

    uploading php

---
+++ consolidation.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Imperial Freight Limited</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ 
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+ 
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="wrapper">
+
+	<!-- start header -->
+		<header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="logo"/>Imperial Freight Limited</a>
+                </div>
+                <div class="navbar-collapse collapse ">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li> 
+						<li><a href="about.php">About Us</a></li>
+						<li class="active"><a href="services.php">Services</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+	</header><!-- end header -->
+	<section id="inner-headline">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2 class="pageTitle">Consolidation</h2>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section id="content">
+		<div class="container content">	
+				
+        <div class="row service-v1 margin-bottom-40">
+            <div class="col-md-6 md-margin-bottom-40">
+               <img class="img-responsive" src="img/consol.jpg" alt="">   
+                       
+            </div>
+            <div>
+            	<p><strong>
+            		We also facilitate cargo consolidation (By Air/Sea) for our customers which includes the transportation of cargo to the stuffing point, stuffing of cargo in container, custom process of shipping documents, use of Premium Air/Shipping lines and we make sure that the cargo will reach at the respective destination well before the time given by our reputed customer.
+            		<br><br>
+					We follow very carefully all the important shipping instructions given and take care of cargo by warehousing it until the shipment is physically effected.
+					<br><br>
+            		</strong>
+            	</p>
+            </div>
+
+            
+        </div>
+        <br>
+
+        <div>
+        	<p>
+        		<strong>Cargo Consolidation Services Include:</strong>
+        	</p>
+        	<br>
+        	<ul>
+				<li>Transportation of cargo to the stuffing point</li><br>
+				
+				<li>Stuffing of cargo in container</li><br>
+				
+				<li>Custom process of shipping documents</li><br>
+				
+				<li>Use of Premium Air/Shipping Lines</li><br>
+
+
+			</ul>
+        </div>
+
+        
+
+        
+        <!-- End Service Blcoks -->
+
+        	<hr class="margin-bottom-50">
+
+
+        
+    	</div>
+    </section>
+	<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="widget">
+					<h5 class="widgetheading">Imperial Freight Ltd</h5>
+					<p>Imperial Freight Limited has been a provider of quality, cost effective, and reliable logistics company since its first established.<br><br>
+				 Through hard work and dedication to our customers, we have positioned ourselves as one of the most reputable logistics providers in Bangladesh with the ability to handle any sort of transportation need supported with our wide network of offices and agents.</p>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="widget">
+					<h5 class="widgetheading">Quick Links</h5>
+					<ul class="link-list">
+						<li><a href="airfreight.php">Air Freight</a></li>
+						<li><a href="seafreight.php">Sea Freight</a></li>
+						<li><a href="roadfreight.php">Road Freight</a></li>
+						<li><a href="combined.php">Combined Sea Air</a></li>
+						<li><a href="consolidation.php">Consolidation</a></li>
+						<li><a href="customclearance.php">Customs Clearance</a></li>
+						<li><a href="warehousing.php">Warehousing</a></li>
+						<li><a href="distribution.php">Distribution</a></li>
+						<li><a href="goh.php">Garments On Hanger</a></li>
+						<li><a href="acs.php">Aircraft Charter Service</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="widget">
+					<h5 class="widgetheading">Contact Us</h5>
+					<address>
+						JC Main Road, Near Silnile tower<br>
+						 Pin-21542 NewYork US.
+					</address>
+					<p>
+						<i class="icon-phone"></i> (123) 456-789 - 1255-12584 <br>
+						<i class="icon-envelope-alt"></i> email@domainname.com
+					</p>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<div id="sub-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="copyright">
+						<p>
+							<span>&copy; Developed by Dhruba Mitra
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<ul class="social-network">
+						<li><a href="https://www.facebook.com/dhruba.mitra.3" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+						
+						<li><a href="https://www.linkedin.com/in/dhruba10414" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	</footer>
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script> 
+<script src="js/portfolio/jquery.quicksand.js"></script>
+<script src="js/portfolio/setting.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
---
+++ services.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Imperial Freight Limited</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ 
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+ 
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="wrapper">
+
+	<!-- start header -->
+		<header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="logo"/>Imperial Freight Limited</a>
+                </div>
+                <div class="navbar-collapse collapse ">
+                    <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li> 
+						<li><a href="about.php">About Us</a></li>
+						<li class="active"><a href="services.php">Services</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+	</header><!-- end header -->
+	<section id="inner-headline">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2 class="pageTitle">Services</h2>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section id="content">
+		<div class="container content">	
+			<div class="row"> 
+							<div class="col-md-12">
+								<div class="about-logo">
+									<h3>Explore all of our services!</h3>
+								</div>
+							</div>
+						</div>		
+        <!-- Service Blcoks -->
+        <div class="row service-v1 margin-bottom-40">
+            <div class="col-md-6 md-margin-bottom-40">
+               <img class="img-responsive" src="img/slides/3.jpg" alt="">   
+                <h2><a href="seafreight.php">Sea Freight</a></h2>
+                       
+            </div>
+            <div class="col-md-6">
+                <img class="img-responsive" src="img/slides/1.jpg" alt="">            
+                <h2><a href="airfreight.php">Air Freight</a></h2>
+            </div>
+            
+        </div>
+
+        <div class="row service-v1 margin-bottom-40">
+            <div class="col-md-6 md-margin-bottom-40">
+               <img class="img-responsive" src="img/Combinepic.jpeg" alt="">   
+                <h2><a href="combined.php">Combined Sea Air</a></h2>
+                       
+            </div>
+            <div class="col-md-6">
+                <img class="img-responsive" src="img/consol.jpg" alt="">            
+                <h2><a href="consolidation.php">Consolidation</a></h2>
+            </div>
+            
+        </div>
+
+        <div class="row service-v1 margin-bottom-40">
+            <div class="col-md-6 md-margin-bottom-40">
+               <img class="img-responsive" src="img/custom.jpg" alt="">   
+                <h2><a href="customclearance.php">Customs Clearance</a></h2>
+                       
+            </div>
+            <div class="col-md-6">
+                <img class="img-responsive" src="img/ware.jpg" alt="">            
+                <h2><a href="warehousing.php">Warehousing</a></h2>
+            </div>
+            
+        </div>
+
+        <div class="row service-v1 margin-bottom-40">
+            <div class="col-md-6 md-margin-bottom-40">
+               <img class="img-responsive" src="img/slides/2.jpg" alt="">   
+                <h2><a href="roadfreight.php">Road Freight</a></h2>
+                       
+            </div>
+            <div class="col-md-6">
+                <img class="img-responsive" src="img/distribution.jpg" alt="">            
+                <h2><a href="distribution.php">Distribution</a></h2>
+            </div>
+            
+        </div>
+
+        <div class="row service-v1 margin-bottom-40">
+            <div class="col-md-6 md-margin-bottom-40">
+               <img class="img-responsive" src="img/goh.jpg" alt="">   
+                <h2><a href="goh.php">Garments on Hanger(GOH)</a></h2>
+                       
+            </div>
+            <div class="col-md-6">
+                <img class="img-responsive" src="img/aircraft.jpg" alt="">            
+                <h2><a href="acs.php">Aircraft Charter Services</a></h2>
+            </div>
+            
+        </div>
+        <!-- End Service Blcoks -->
+
+        <hr class="margin-bottom-50">
+
+
+        
+    </div>
+    </section>
+	<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="widget">
+					<h5 class="widgetheading">Imperial Freight Ltd</h5>
+					<p>Imperial Freight Limited has been a provider of quality, cost effective, and reliable logistics company since its first established.<br><br>
+				 Through hard work and dedication to our customers, we have positioned ourselves as one of the most reputable logistics providers in Bangladesh with the ability to handle any sort of transportation need supported with our wide network of offices and agents.</p>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="widget">
+					<h5 class="widgetheading">Quick Links</h5>
+					<ul class="link-list">
+						<li><a href="airfreight.php">Air Freight</a></li>
+						<li><a href="seafreight.php">Sea Freight</a></li>
+						<li><a href="roadfreight.php">Road Freight</a></li>
+						<li><a href="combined.php">Combined Sea Air</a></li>
+						<li><a href="consolidation.php">Consolidation</a></li>
+						<li><a href="customclearance.php">Customs Clearance</a></li>
+						<li><a href="warehousing.php">Warehousing</a></li>
+						<li><a href="distribution.php">Distribution</a></li>
+						<li><a href="goh.php">Garments On Hanger</a></li>
+						<li><a href="acs.php">Aircraft Charter Service</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="widget">
+					<h5 class="widgetheading">Contact Us</h5>
+					<address>
+						JC Main Road, Near Silnile tower<br>
+						 Pin-21542 NewYork US.
+					</address>
+					<p>
+						<i class="icon-phone"></i> (123) 456-789 - 1255-12584 <br>
+						<i class="icon-envelope-alt"></i> email@domainname.com
+					</p>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<div id="sub-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="copyright">
+						<p>
+							<span>&copy; Developed by Dhruba Mitra
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<ul class="social-network">
+						<li><a href="https://www.facebook.com/dhruba.mitra.3" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+						
+						<li><a href="https://www.linkedin.com/in/dhruba10414" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	</footer>
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script> 
+<script src="js/portfolio/jquery.quicksand.js"></script>
+<script src="js/portfolio/setting.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
