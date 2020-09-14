4c595d93ded8b835e2587d8f51670009be42e2a2
Dhruba Mitra <dhruba10414@gmail.com>
2020-09-14 22:57:41 +0600

    logo change

--- customclearance.php
+++ customclearance.php
@@ -30,7 +30,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                   <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="logo"/>Imperial Freight Limited</a>
+                   <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="logo"/></a>
                 </div>
                 <div class="navbar-collapse collapse ">
                     <ul class="nav navbar-nav">
